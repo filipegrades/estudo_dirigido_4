13954c77339997b3aed066ec0f11f600b331c67f
filipegrades <filipe.grades@estudante.ufjf.br>
2026-05-21 10:32:57 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,43 @@
+
+#include "driverlib.h"
+#include "device.h"
+#include "board.h"
+
+//
+// Main
+//
+void main(void)
+{
+    // Device Initialization
+    Device_init();
+
+    //
+    // Initializes PIE and clears PIE registers. Disables CPU interrupts.
+    //
+    Interrupt_initModule();
+
+    //
+    // Initializes the PIE vector table with pointers to the shell Interrupt
+    // Service Routines (ISR).
+    //
+    Interrupt_initVectorTable();
+
+	Board_init();
+
+    //
+    // Enable Global Interrupt (INTM) and realtime interrupt (DBGM)
+    //
+    EINT;
+    ERTM;
+
+    while(1)
+    {
+    }	
+	
+}
+
+
+
+//
+// End of File
+//
